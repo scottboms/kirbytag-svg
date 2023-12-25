dc38f5f88c721fb900de87f33abd755bc48cd641
Scott Boms <scottboms@gmail.com>
2023-12-25 13:58:36 -0800

    Added new attributes to define a wrapper object, class, and role surrounding SVG output. Updated to version 1.0.1.

--- README.md
+++ README.md
@@ -4,13 +4,23 @@ A kirbytag for processing SVGs within kirbytags and outputting inline SVG code.
 
 ## Installation
 
-### Download
+## Download
 
 Download and copy this repository to `/site/plugins/kirbytag-svg`.
 
-### Usage
+## Usage
 
 `(svg: yourfile.svg)`
+
+Optionally, you can specify an element to wrap the SVG along with class and role attributes that will be applied to that element. If `class` or `role` attributes are included but no `wrapper` element, these will be ignored.
+
+### Optional Tag Attributes
+
+* `wrapper`: A wrapper element to surround the SVG when output in your template [optional]
+* `class`: A CSS class/classes to append to the wrapper element [optional]
+* `role`: A role attribute appended to the wrapper element [optional]
+
+Example usage: `(svg: yourfile.svg wrapper: figure class: svg role: img)` 
 
 ## Compatibility
 
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 /**
  * Kirby SVG KirbyTag
  *
- * @version 1.0.0
+ * @version 1.0.1
  * @author Scott Boms <plugins@scottboms.com>
  * @copyright Scott Boms <plugins@scottboms.com>
  * @link https://github.com/scottboms/kirbytag-svg
@@ -15,9 +15,32 @@
 Kirby::plugin('scottboms/kirbytag-svg', [
   'tags' => [
     'svg' => [
+      'attr' => [
+        'wrapper',
+        'class',
+        'role'
+      ],
       'html' => function($tag) {
-        $icon_url = $tag->value();
-        return svg($icon_url);
+        $html = '';
+        // Variables to hold tag options
+        $svg_url = $tag->svg;
+        $wrapper = $tag->wrapper;
+        $class = $tag->class;
+        $role = $tag->role;
+
+        if($wrapper == '') {
+          // If no wrapper attribute is found, output the SVG without a
+          // wrapper element and ignore associated options if present
+          $html .= svg($svg_url);
+        } else {
+          // If a wrapper option has been specified, prepend the svg element
+          // with that element and its associated class and role options
+          $html .= '<' . $wrapper . ' class="' . $class . '" role="' . $role . '">';
+          $html .= svg($svg_url);
+          $html .= '</' . $wrapper . '>';
+        }
+
+        return $html;
       }
     ]
   ]
